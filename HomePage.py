e5342629930673d7cfc7dbf95f85664cd7bded91
Will <wiliamahern@gmail.com>
2025-11-18 11:44:18 -0800

    Beginning Edits.1

--- HomePage.py
+++ HomePage.py
@@ -1,5 +1,30 @@
-import pygame as pg 
+import pygame as pg
+
+
+# This is the HomePage class for the game. It will display the home page of the game and allow the user to start the game.
+pg.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+#Yo if you can change this to to activate when main is on, that'd be great.
+Run = True
+
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+while Run:
+    
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            Run = False
+
+pg.quit()
+        
+            
+
+
+    
 
 class HomePage:
     def __init__(self, screen):
-        self.screen = screen
+        pass
+        
